22539f11732c225615d862c779ac3b0b4d6933ab
= <=>
2015-04-26 21:07:40 +0800

    give up

--- static/js/custom.js
+++ static/js/custom.js
@@ -67,28 +67,3 @@ $(document).ready(function(){
         };
     });
 })
-
-// 返回顶部
-function goTopEx() {  
-    var obj = document.getElementById("goTopBtn");  
-    function getScrollTop() {  
-        return document.documentElement.scrollTop + document.body.scrollTop;  
-    }  
-    function setScrollTop(value) {  
-        if (document.documentElement.scrollTop) {  
-            document.documentElement.scrollTop = value;  
-        } else {  
-            document.body.scrollTop = value;  
-        }  
-    }  
-    window.onscroll = function() {  
-        getScrollTop() > 0 ? obj.style.display = "": obj.style.display = "none";  
-    }  
-    obj.onclick = function() {  
-        var goTop = setInterval(scrollMove, 10);  
-        function scrollMove() {  
-            setScrollTop(getScrollTop() / 1.1);  
-            if (getScrollTop() < 1) clearInterval(goTop);  
-        }  
-    }  
-}
